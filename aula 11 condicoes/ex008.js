35c82822d246efbf5c2feaf4fb894a7eeea7fb98
PedroPauloDias <89479629+PedroPauloDias@users.noreply.github.com>
2022-01-31 10:07:41 -0300

    desafio java

---
+++ aula 11 condicoes/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`a velocidade do seu carro é ${vel} km/h`) 
+if (vel > 60) {// condicao simples
+    console.log(`você ultrapassou a velocidade permitida. Multado!`)
+}
+console.log(`Dirija sempre usando cinto de segurança !`)
