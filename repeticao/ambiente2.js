76488096f99b96a45d7cd638889bca6398c00448
PedroPauloDias <89479629+PedroPauloDias@users.noreply.github.com>
2022-02-01 11:49:20 -0300

    repticoes em java

---
+++ repeticao/ambiente2.js
@@ -0,0 +1,27 @@
+// usando o while , teste logico no inicio da repetição
+
+
+/*var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++
+}*/
+
+// usando o do while , com o teste logico depois da execução do bloco e o teste logico no finall
+
+
+/*var c = 1
+ do {
+    console.log(c)
+    c++
+}while (c <= 5)*/
+
+// usando o for 1- inicialização , 2- teste logico 3- incremento, primeiro a inic,depois o teste logico a na volta o incr
+// extrutura for
+
+
+console.log('vai começar !')
+for (var c = 1; c <= 5; c++){
+    console.log(c)
+}
+console.log('FIM !')
